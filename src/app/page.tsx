756606ee3283856a91394b1d3d452c831f359048
Alana Wesly <awesly@hawaii.edu>
2025-03-27 10:13:04 -1000

    draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ const Home = () => (
     <Container id="landing-page" fluid className="py-3">
       <Row className="align-middle text-center">
         <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
+          
         </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
+        <Col xs={4}>
+          
+        </Col>
+        <Col xs={4} className="d-flex flex-column justify-content-center">
           <h1>Welcome to this template</h1>
           <p>Now get to work and modify this app!</p>
         </Col>
